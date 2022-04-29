c7a0c8275fa21a6c54aa21accc8ec18653ebd65e
Abigail <abigail.tyrell@noaa.gov>
2022-04-29 09:31:51 -0400

    try fixing geom problem

--- temperature.R
+++ temperature.R
@@ -6,6 +6,10 @@ crs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 mab <- NEesp::shape %>%
   dplyr::select(STRATA, geometry) %>%
   sf::st_transform(proj4string = crs) %>% 
+  # try geom fix?
+  dplyr::mutate(geometry = geometry %>% 
+                  s2::s2_rebuild() %>%
+                  sf::st_as_sfc()) %>%
   dplyr::summarise(geometry = sf::st_union(geometry)) %>%  
   sf::st_crop(y = c(xmin = -80, xmax = -69, 
                     ymax = 41.5, ymin =  35.8327))
