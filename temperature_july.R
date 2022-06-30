5e080c326bbb28781da04fe66bf00aaa1e0ce121
Abigail <abigail.tyrell@noaa.gov>
2022-06-30 10:29:18 -0400

    update area calculation

--- temperature_july.R
+++ temperature_july.R
@@ -30,7 +30,7 @@ for(j in years) {
   # R can't open the file (will have to do this in a gh action...)
   # download file manually for testing on desktop
   
-  # name <- paste0(j, ".nc")
+   # name <- paste0(j, ".nc")
   name <- "test.nc"
   
   data <- ecopull::nc_to_raster(nc = name, varname = 'sst')
@@ -43,57 +43,37 @@ for(j in years) {
     message(j, " does not have a full year of data! skipping!")
   } else {
   
+    # filter to just july
+    july <- c(paste0("X", j, ".07.0", 1:9),
+              paste0("X", j, ".07.", 10:31))
+    
   # crop to MAB ----
-  ndays <- raster::nlayers(data) # account for leap years
+  # ndays <- raster::nlayers(data) # account for leap years
   
-  mab_temp <- raster::mask(x = data[[1:180]], 
+  mab_temp <- raster::mask(x = data[[july]], 
                            mask = mab)
-  mab_temp2 <- raster::mask(x = data[[181:ndays]], 
-                            mask = mab)
-  message("cropped to MAB...")
+  message("cropped to July MAB...")
+
+  # calculate total area ----
+  total_area <- raster::area(mab_temp, na.rm = TRUE) %>%
+    raster::as.data.frame(xy = TRUE) %>%
+    dplyr::select(-c(x, y)) %>%
+    colSums(na.rm = TRUE)
   
-  # create dataframes ----
-  rast_mab_df <- raster::as.data.frame(mab_temp, xy = TRUE)
-  rast_mab_df2 <- raster::as.data.frame(mab_temp2, xy = TRUE)
-  message("created data frames...")
+  # calculate area above 22C ----
+  mab_temp[mab_temp[] < 22 ] <- NA
   
-  # calculate proportion above 22C by day ----
-  names <- c()
-  value <- c()
-  for(i in 3:ncol(rast_mab_df)){
-    new_dat <- rast_mab_df[,i] %>%
-      tibble::as_tibble() %>%
-      tidyr::drop_na()
-    names[i-2] <- colnames(rast_mab_df[i])
-    value[i-2] <- nrow(new_dat %>% dplyr::filter(value > 22)) / nrow(new_dat)
-  }
-  mab_prop <- tibble::tibble(names, value, region = "MAB")
+  july_area <- raster::area(mab_temp, na.rm = TRUE) %>%
+    raster::as.data.frame(xy = TRUE) %>%
+    dplyr::select(-c(x, y)) %>%
+    colSums(na.rm = TRUE)
   
-  names <- c()
-  value <- c()
-  for(i in 3:ncol(rast_mab_df2)){
-    new_dat <- rast_mab_df2[,i] %>%
-      tibble::as_tibble() %>%
-      tidyr::drop_na()
-    names[i-2] <- colnames(rast_mab_df2[i])
-    value[i-2] <- nrow(new_dat %>% dplyr::filter(value > 22)) / nrow(new_dat)
-  }
-  mab_prop2 <- tibble::tibble(names, value, region = "MAB")
+  # calculate proportion ----
   
-  mab_prop <- rbind(mab_prop, mab_prop2) %>%
-    dplyr::mutate(Year = stringr::str_extract(names, pattern = "\\d{4}"), 
-                  names = stringr::str_remove(names, pattern = "X"), 
-                  DOY = lubridate::as_date(names),
-                  week = lubridate::week(DOY),
-                  month = lubridate::month(DOY),
-                  value = as.numeric(value))
+  props <- july_area/total_area
+  this_prop <- mean(props)
   
-  this_prop <- mab_prop %>%
-    dplyr::filter(region == "MAB",
-                  month == 7) %>%
-    dplyr::arrange(DOY)
-  
-  prop_july <- c(prop_july, mean(this_prop$value))
+  prop_july <- c(prop_july, this_prop)
   message("calculated proportion...")
   }
   message(paste("done with", j))
