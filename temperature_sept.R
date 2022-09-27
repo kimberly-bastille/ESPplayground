5237120e7444c3aa7cfad2e36583c8c1b13c05a6
Abigail <abigail.tyrell@noaa.gov>
2022-09-27 09:22:05 -0400

    try fixing raster stack names

--- temperature_sept.R
+++ temperature_sept.R
@@ -59,7 +59,7 @@ for(j in years) {
   # calculate mean temp for each location
   
   mean_temp <- raster::calc(na_temp, mean)
-  names(mean_temp) <- i
+  names(mean_temp) <- j
   
   if(is.null(temp_out)) {
     temp_out <- mean_temp
